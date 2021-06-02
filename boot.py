87c709e333e328ec23debf4c7ec23502e8996dd6
Laurent Toutain <ltn22@github.com>
2021-06-02 14:28:16 +0200

    fix addr

--- boot.py
+++ boot.py
@@ -3,6 +3,9 @@ import time
 
 # setup as a station
 wlan = network.WLAN(mode=network.WLAN.STA)
+
+wlan.ifconfig(config=('10.51.0.241', '255.255.255.0', '10.51.0.1', '192.108.119.134'))
+
 wlan.connect('RSM-B25', auth=(network.WLAN.WEP, 'df72f6ce24'))
 
 while not wlan.isconnected():
