f2bfd216d0fa551b958a0307490459b621a6d602
Kaustubh Joshi <kaustubh@dhcp-salsa-i175.rennes.enst-bretagne.fr>
2022-01-22 22:35:50 +0100

    changes in the management web app

--- msc_project/AdminWebapp/mongodb_pymongo.py
+++ msc_project/AdminWebapp/mongodb_pymongo.py
@@ -5,7 +5,7 @@ from wtforms.validators import InputRequired
 from flask_pymongo import PyMongo
 import os
 from pymongo import MongoClient
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil import parser
 
 
