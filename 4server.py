a3cb7ba5d2bf883421f5a57c08e6921fb98abcea
Kaustubh Joshi <kaustubh@dhcp-salsa-i65.rennes.enst-bretagne.fr>
2022-01-22 13:47:44 +0100

    testing server side changes

--- 4server.py
+++ 4server.py
@@ -153,8 +153,8 @@ class watering_info(resource.Resource):
         for d in client.green_wall.devices.find():
             humidity_level = {}
             humidity_level['device_name'] = d['name']
-            latest_measures = client.green_wall.devicemeasures.find({"device_id":d['_id']},{"sort": {"natural": -1}}).limit(1)[0]
-            print ("KKKKKKK", latest_measures)
+            latest_measures = client.green_wall.devicemeasures.find({"device_id":d['_id']}).sort([('recorded_at', -1)]).limit(1)
+            print("KKKKK", latest_measures)
             for ms in latest_measures['measures']:
                 ic += 1
                 totalh += ms
