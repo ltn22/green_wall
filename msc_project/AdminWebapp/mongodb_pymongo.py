b2e9028dd06b9c5516e37362a7d2e1ec68e6c5eb
Kaustubh Joshi <kaustubh@dhcp-salsa-i175.rennes.enst-bretagne.fr>
2022-01-23 18:36:44 +0100

    changes in the management web app

--- msc_project/AdminWebapp/mongodb_pymongo.py
+++ msc_project/AdminWebapp/mongodb_pymongo.py
@@ -25,36 +25,36 @@ app.config['SECRET_KEY'] = os.urandom(24)
 #From class from WTForms to handle adding and Updating Devices
 #=============================================================
 class AddForm(FlaskForm):
-	name = StringField('name', validators = [InputRequired()])
-	#unique_id = StringField('unique_id', validators = [InputRequired()])
+    name = StringField('name', validators = [InputRequired()])
+    #unique_id = StringField('unique_id', validators = [InputRequired()])
 
 
 #=============================================================
 #From class from WTForms to handle adding and Updating Sensors
 #=============================================================
 class AddSensorForm(FlaskForm):
-	name = StringField('name', validators = [InputRequired()])
-	stype = StringField('type', validators = [InputRequired()])
-	position_x = IntegerField('pos_X', validators = [InputRequired()])
-	position_y = IntegerField('pos_Y', validators = [InputRequired()])
+    name = StringField('name', validators = [InputRequired()])
+    stype = StringField('type', validators = [InputRequired()])
+    position_x = IntegerField('pos_X', validators = [InputRequired()])
+    position_y = IntegerField('pos_Y', validators = [InputRequired()])
 
 #===============================
 #Homepage
 #===============================
 @app.route('/')
 def home():
-	return render_template("home.html")
+    return render_template("home.html")
 
 @app.route('/devices')
 def devices():
-	device_list = list(mongo.green_wall.devices.find())
-	for d in device_list:
-		device_last_updated_at = datetime.datetime.strptime(d['last_updated_at'], '%Y-%m-%d %H:%M:%S.%f')
-		if device_last_updated_at < datetime.datetime.utcnow() - timedelta(seconds=300):
-			d['status'] = 'Inactive'
-		else:
-			d['status'] = 'Active'			
-	return render_template("devices_result.html",device_list=device_list)
+    device_list = list(mongo.green_wall.devices.find())
+    for d in device_list:
+        device_last_updated_at = datetime.datetime.strptime(d['last_updated_at'], '%Y-%m-%d %H:%M:%S.%f')
+        if device_last_updated_at < datetime.datetime.utcnow() - timedelta(seconds=300):
+            d['status'] = 'Inactive'
+        else:
+            d['status'] = 'Active'			
+    return render_template("devices_result.html",device_list=device_list)
 
 
 
@@ -79,11 +79,11 @@ class JSONEncoder(json.JSONEncoder):
 
 @app.route('/updateform')
 def updateform():
-	id = request.args.get('id')
-	devices = mongo.green_wall.devices
-	result_id = devices.find_one({'_id':ObjectId(id)})
-	form = AddForm(name=result_id['name'])
-	return render_template("update_device.html", form=form, id = id)
+    id = request.args.get('id')
+    devices = mongo.green_wall.devices
+    result_id = devices.find_one({'_id':ObjectId(id)})
+    form = AddForm(name=result_id['name'])
+    return render_template("update_device.html", form=form, id = id)
 
 #===================================
 #Updating Device in the collection
@@ -91,11 +91,11 @@ def updateform():
 from bson import json_util
 @app.route('/update/<id>', methods=["POST"])
 def update(id):
-	devices = mongo.green_wall.devices
-	form = AddForm()
-	if form.validate_on_submit():
-		result = devices.update_one({'_id':ObjectId(id)},{'$set':{'name':form.name.data}})
-	return redirect(url_for('devices'))
+    devices = mongo.green_wall.devices
+    form = AddForm()
+    if form.validate_on_submit():
+        result = devices.update_one({'_id':ObjectId(id)},{'$set':{'name':form.name.data}})
+    return redirect(url_for('devices'))
 
 #===================================
 #deleting Device in the collection
@@ -103,29 +103,29 @@ def update(id):
 
 @app.route('/delete/<id>')
 def delete(id):
-	devices = mongo.green_wall.devices
-	#delete all the sensors associated with this device
-	mongo.green_wall.sensors.remove({"device_id":ObjectId(id)})
-	#now delete the device itself
-	delete_record = devices.delete_one({'_id':ObjectId(id)})
-	return redirect(url_for('devices'))
+    devices = mongo.green_wall.devices
+    #delete all the sensors associated with this device
+    mongo.green_wall.sensors.remove({"device_id":ObjectId(id)})
+    #now delete the device itself
+    delete_record = devices.delete_one({'_id':ObjectId(id)})
+    return redirect(url_for('devices'))
 
 
-		
+        
 #===================================
 #List all the sensors for a device : GET
 #===================================
 
 @app.route('/sensors/<device_id>')
 def sensors(device_id):
-	sensor_list = list(mongo.green_wall.sensors.find({"device_id":ObjectId(device_id)}))
-	for s in sensor_list:
-		sensor_last_updated_at = datetime.datetime.strptime(s['last_updated_at'], '%Y-%m-%d %H:%M:%S.%f')
-		if sensor_last_updated_at < datetime.datetime.utcnow() - timedelta(seconds=300):
-			s['status'] = 'Inactive'
-		else:
-			s['status'] = 'Active'
-	return render_template("sensors_result.html",sensor_list=sensor_list)
+    sensor_list = list(mongo.green_wall.sensors.find({"device_id":ObjectId(device_id)}))
+    for s in sensor_list:
+        sensor_last_updated_at = datetime.datetime.strptime(s['last_updated_at'], '%Y-%m-%d %H:%M:%S.%f')
+        if sensor_last_updated_at < datetime.datetime.utcnow() - timedelta(seconds=300):
+            s['status'] = 'Inactive'
+        else:
+            s['status'] = 'Active'
+    return render_template("sensors_result.html",sensor_list=sensor_list)
 
 
 #===================================
@@ -134,10 +134,10 @@ def sensors(device_id):
 
 @app.route('/updatesensorform')
 def updatesensorform():
-	id = request.args.get('id')
-	result_id = mongo.green_wall.sensors.find_one({'_id':ObjectId(id)})
-	form = AddSensorForm(name=result_id['name'],stype=result_id['type'],position_x=result_id['pos_X'], position_y=result_id['pos_Y'])
-	return render_template("update_sensor.html", form=form, id = id)
+    id = request.args.get('id')
+    result_id = mongo.green_wall.sensors.find_one({'_id':ObjectId(id)})
+    form = AddSensorForm(name=result_id['name'],stype=result_id['type'],position_x=result_id['pos_X'], position_y=result_id['pos_Y'])
+    return render_template("update_sensor.html", form=form, id = id)
 
 #===================================
 #Updating Sensor in the Database: POST
@@ -145,11 +145,11 @@ def updatesensorform():
 from bson import json_util
 @app.route('/updatesensor/<id>', methods=["POST"])
 def updatesensor(id):
-	device =  mongo.green_wall.sensors.find_one({'_id':ObjectId(id)})
-	form = AddSensorForm()
-	if form.validate_on_submit():
-		result = sensors.update_one({'_id':ObjectId(id)},{'$set':{'name':form.name.data, 'type': form.stype.data, 'pos_X': form.position_x.data, 'pos_Y': form.position_y.data}})
-	return redirect(url_for('sensors',device_id=device['_id']))
+    device =  mongo.green_wall.sensors.find_one({'_id':ObjectId(id)})
+    form = AddSensorForm()
+    if form.validate_on_submit():
+        result = sensors.update_one({'_id':ObjectId(id)},{'$set':{'name':form.name.data, 'type': form.stype.data, 'pos_X': form.position_x.data, 'pos_Y': form.position_y.data}})
+    return redirect(url_for('sensors',device_id=device['_id']))
 
 #===================================
 #deleting Sensor in the collection
@@ -157,13 +157,13 @@ def updatesensor(id):
 
 @app.route('/deletesensor/<id>')
 def deletesensor(id):
-	device = mongo.green_wall.sensors.find_one({'_id':ObjectId(id)})
-	delete_record = mongo.green_wall.sensors.delete_one({'_id':ObjectId(id)})
-  	return redirect(url_for('sensors',device_id=device['_id']))
+    device = mongo.green_wall.sensors.find_one({'_id':ObjectId(id)})
+    delete_record = mongo.green_wall.sensors.delete_one({'_id':ObjectId(id)})
+    return redirect(url_for('sensors',device_id=device['_id']))
 
 
 if __name__=='__main__':
-	app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
 
 
 
