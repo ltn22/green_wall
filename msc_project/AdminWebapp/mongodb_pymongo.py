0e515981034da8fce04be904f71b28d2af9ccc9a
Kaustubh Joshi <kaustubh@dhcp-salsa-i175.rennes.enst-bretagne.fr>
2022-01-23 18:13:45 +0100

    changes in the management web app

--- msc_project/AdminWebapp/mongodb_pymongo.py
+++ msc_project/AdminWebapp/mongodb_pymongo.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template,url_for, redirect, jsonify, request, session
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, IntegerField
 from wtforms.validators import InputRequired
 from flask_pymongo import PyMongo
 import os
