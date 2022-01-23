6165539fd6b6215faf35666689314af6ab537244
Kaustubh Joshi <kaustubh@Kaustubhs-MacBook-Air.local>
2022-01-23 20:25:18 +0100

    changes in the management web app

--- msc_project/AdminWebapp/mongodb_pymongo.py
+++ msc_project/AdminWebapp/mongodb_pymongo.py
@@ -119,13 +119,14 @@ def delete(id):
 @app.route('/sensors/<device_id>')
 def sensors(device_id):
     sensor_list = list(mongo.green_wall.sensors.find({"device_id":ObjectId(device_id)}))
+    device_name = mongo.green_wall.devices.find_one({"device_id":ObjectId(device_id)},{'name':1})
     for s in sensor_list:
         sensor_last_updated_at = datetime.datetime.strptime(s['last_updated_at'], '%Y-%m-%d %H:%M:%S.%f')
         if sensor_last_updated_at < datetime.datetime.utcnow() - timedelta(seconds=300):
             s['status'] = 'Inactive'
         else:
             s['status'] = 'Active'
-    return render_template("sensors_result.html",sensor_list=sensor_list)
+    return render_template("sensors_result.html",sensor_list=sensor_list, device_name = device_name)
 
 
 #===================================
