8a3d04bb5e4933353f94b9a4afa32563daa122da
Kaustubh Joshi <kaustubh@dhcp-salsa-i175.rennes.enst-bretagne.fr>
2022-01-24 09:59:52 +0100

    some changes for css

--- msc_project/AdminWebapp/webapp_controller.py
+++ msc_project/AdminWebapp/webapp_controller.py
@@ -126,8 +126,9 @@ def sensors(device_id):
             s['status'] = 'Inactive'
         else:
             s['status'] = 'Active'
-        last_recorded_value = list(mongo.green_wall.measurements.find({"sensor_id":s['_id']}).sort([('recorded_at', -1)]).limit(1))[0]    
-    return render_template("sensors_result.html",sensor_list=sensor_list, device_name = device_name['name'], last_value = last_recorded_value['value'])
+        last_recorded_value = list(mongo.green_wall.measurements.find({"sensor_id":s['_id']}).sort([('recorded_at', -1)]).limit(1))[0]  
+        s['last_value'] = last_recorded_value  
+    return render_template("sensors_result.html",sensor_list=sensor_list, device_name = device_name['name'])
 
 
 #===================================
